4bee1f7c6d38b29f05ecb0b2107d10cefba99351
anabellchan <anbellchan@gmail.com>
2021-01-12 18:57:32 -0800

    Change service endpoint. Add morgan middleware to server.

--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 require('newrelic');
 
 const PORT = 3000;
 const app = express();
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/products', createProxyMiddleware({
-  target: 'http://localhost:3002',
+  target: 'http://54.201.100.200:3002',
   changeOrigin: true,
 }));
 
 app.use('/api', createProxyMiddleware({
-  target: 'http://localhost:3002',
+  target: 'http://54.201.100.200:3002',
   changeOrigin: true,
 }));
 
